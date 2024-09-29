542ae1d6a6efaae387b74e3eaeb4fe1226a6a5df
Thilo Hirscher <176391490+TeamPlatinum1@users.noreply.github.com>
2024-09-29 21:39:10 +0200

    Add first step for UI and Logic

---
+++ javascript.js
@@ -0,0 +1,43 @@
+const errorCodes = {
+    NO_SUCH_OPERATOR: "NO_SUCH_OPERATOR",
+}
+
+class operation {
+    constructor(num1, num2, operator){
+        this.num1 = num1;
+        this.num2 = num2;
+        this.operator = operator;
+    }
+}
+
+function operate(operation){
+    switch(operation.operator){
+        case "+":
+            return add(operation.num1, operation.num2);
+        case "-":
+            return subtract(operation.num1, operation.num2);
+        case "*":
+            return multiply(operation.num1, operation.num2);
+        case "/":
+            return divide(operation.num1, operation.num2);
+        default:
+            return errorCodes.NO_SUCH_OPERATOR;
+    }
+}
+
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
